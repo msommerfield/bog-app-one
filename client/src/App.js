96b1e70026533fee87019ccbcc47925501a34e3b
Mandy <mandysommerfield@gmail.com>
2019-03-19 15:16:49 -0400

    added switches and routes to app dot js

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Creatures from './components/Creatures'
+import SingleCreature from './components/SingleCreature'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Creatures}/>
+            <Route path="/:id" component={SingleCreature} />
+          </Switch>
+        </div>
+      </Router>
+    )
   }
 }
 
