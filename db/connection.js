eb4940b8354ea2e0fe0dc6adefb570ecfe64b019
Mandy <mandysommerfield@gmail.com>
2019-03-19 11:06:06 -0400

    set up Creature model

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 if(process.env.MONGODB_URI) {
     mongoose.connect(process.env.MONGODB_URI)
 } else {
-    mongoose.connect('mongodb://localhost/idea-board')
+    mongoose.connect('mongodb://localhost/bog-app-one')
 }
 
 mongoose.connection.on('error', (err) => {
--- server.js
+++ server.js
@@ -9,5 +9,5 @@ app.get('/', (req,res) => {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log("Magic happening on port " + PORT);
+  console.log(`Magic happening on port  + ${PORT}`);
 })
