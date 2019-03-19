aa7d51e866b1ae5e8d0b0bbde5c436b763649a51
Mandy <mandysommerfield@gmail.com>
2019-03-19 11:13:23 -0400

    added seed file

---
+++ db/seeds.js
@@ -0,0 +1,12 @@
+const Creature = require('../models/Creature.js')
+
+const saved = async () => {
+    await Creature.deleteMany()
+    const fred = new Creature({name: 'Fred', description: 'dawg'})
+    await fred.save()
+    const sadie = new Creature({name: 'Sadie', description: 'pup'})
+    await sadie.save()
+    const emily = new Creature({name: 'Emily', description: 'sister' })
+}
+
+saved()
